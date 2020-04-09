c6bb81c4167013a8fd9ec37d9910e128a6cea2af
Abhay <abhayc.008@gmail.com>
2020-04-09 15:04:55 +0530

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,10 @@
-<Project Sdk="Microsoft.NET.Sdk">
+public class Example 
+{
 
-  <PropertyGroup>
-    <TargetFramework>netcoreapp3.1</TargetFramework>
-  </PropertyGroup>
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
 
-  <ItemGroup>
-    <Compile Remove="Migrations\20200401125753_ModifiedIdentity1.cs" />
-    <Compile Remove="Migrations\20200401125753_ModifiedIdentity1.Designer.cs" />
-    <Compile Remove="Migrations\20200401141635_ExtendIdentity_BucketFolder.cs" />
-    <Compile Remove="Migrations\20200401141635_ExtendIdentity_BucketFolder.Designer.cs" />
-  </ItemGroup>
+	}
 
-  <ItemGroup>
-    <PackageReference Include="Microsoft.AspNetCore.Identity.EntityFrameworkCore" Version="3.1.3" />
-    <PackageReference Include="Microsoft.EntityFrameworkCore.SqlServer" Version="3.1.3" />
-    <PackageReference Include="Microsoft.EntityFrameworkCore.Tools" Version="3.1.3">
-      <PrivateAssets>all</PrivateAssets>
-      <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
-    </PackageReference>
-  </ItemGroup>
-
-</Project>
+}

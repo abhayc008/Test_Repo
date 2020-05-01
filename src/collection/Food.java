8e62fa72dbb1b705de0176d91479393897e33011
Abhay <abhayc.008@gmail.com>
2020-05-01 18:42:25 +0530

    Create Food.java

---
+++ src/collection/Food.java
@@ -0,0 +1,13 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Food 
+{
+  int foodId;
+  String foodName;
+  String foodType;
+  String foodCategory;
+  int foodPrice;
+}
